1463589c0e58bb6c2dcb0c0039fa9e716559bd54
Sam Windell <info@frozenplain.com>
2024-01-09 17:19:40 +0000

    change display off timeout

--- home.nix
+++ home.nix
@@ -307,7 +307,7 @@ in
     systemdTarget = "hyprland-session.target";
     timeouts = [
       {
-        timeout = 60;
+        timeout = 90;
         command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
         resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       }
