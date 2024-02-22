917952769f9f029aab4f8d626d18a39dbc17ce78
Sam Windell <info@frozenplain.com>
2024-02-22 17:10:17 +0000

    disable lsp log

--- nvim/nvim-init.lua
+++ nvim/nvim-init.lua
@@ -22,6 +22,7 @@ vim.opt.linebreak = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
+vim.lsp.set_log_level("OFF")
 
 if vim.loop.os_uname().sysname == "Darwin" then
     vim.keymap.set('i', "<a-3>", "#")
@@ -716,7 +717,6 @@ telescope.setup({
             override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true,    -- override the file sorter
             case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
         },
         smart_open = {
             match_algorithm = "fzf",
