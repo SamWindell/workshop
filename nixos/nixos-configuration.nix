45a974f9c7ac6f0a7c117023b617d3b337ee6407
Sam Windell <info@frozenplain.com>
2024-08-24 11:04:20 +0100

    comments

--- nixos/nixos-configuration.nix
+++ nixos/nixos-configuration.nix
@@ -58,8 +58,8 @@
     xwayland.enable = true;
   };
 
-  # Configure keymap in X11
   services.xserver = {
+    # Configure keymap in X11
     xkb = {
       variant = "";
       layout = "us";
@@ -88,25 +88,6 @@
     jack.enable = true;
     wireplumber.enable = true;
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
